76f6a6a7a2d05bdf7bd2f15889982ac868c0735d
Francisco Javier Lopez Tirado <wkm.jtirado@terceiros.mag.com.br>
2023-12-14 23:24:52 -0300

    create Site da Vida Segura

---
+++ main.js
@@ -0,0 +1,43 @@
+function redirectLogin() {
+    window.location.href = "/login.html";
+}
+
+let navbarDesktop = document.querySelectorAll("#navbar-desktop");
+let navBarMovil = document.querySelectorAll("#navBarMovil");
+let headerMovil = document.querySelectorAll("#headerMovil");
+
+function atualizarEstilo() {
+    let anchoPantalla = window.innerWidth;
+
+    if (anchoPantalla < 530) {
+        navbarDesktop.style.display = "none";
+        headerMovil.style.opacity = "100%";
+    } else {
+        navbarDesktop.style.display = "block";
+        navBarMovil.style.left = "-250px";
+        headerMovil.style.opacity = "0%";
+    }
+}
+
+atualizarEstilo();
+
+window.addEventListener("resize", actualizarEstilo);
+
+document.addEventListener("DOMContentLoaded", function () {
+  var navBarMovil = document.querySelector("#navBarMovil");
+  var iconHamburger = document.querySelector(".nav-bar-movil img");
+
+  iconHamburger.addEventListener("click", function (event) {
+      event.stopPropagation();
+      toggleNavBar();
+  });
+
+  document.addEventListener("click", function () {
+      navBarMovil.classList.remove("show-nav");
+  });
+});
+
+function toggleNavBar() {
+  var navBarMovil = document.getElementById("navBarMovil");
+  navBarMovil.classList.toggle("show-nav");
+}
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Vida Segura Website
+Este repositório contém o código-fonte para o site da Vida Segura. O site foi projetado para fornecer informações sobre seguros de vida e previdência, além de oferecer serviços aos clientes.
+
+
+## Tecnologias Utilizadas
+HTML5
+CSS3
+Bootstrap 5.3.2: Framework CSS para o desenvolvimento de interfaces web responsivas.
+JavaScript: Utilizado para adicionar interatividade e comportamentos dinâmicos.
+
+## Clonar o Repositório:
+```bash
+git clone https://github.com/fjavier02/FIAP-RM553161.git
+```
+
+## Abrir o Site Localmente:
+
+Abra o arquivo `index.html` em seu navegador web.
